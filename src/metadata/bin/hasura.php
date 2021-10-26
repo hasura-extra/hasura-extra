bb7f95b88922a6a13688a94319411911880a388e
Minh Vuong <vuongxuongminh@gmail.com>
2021-10-26 13:49:46 +0700

    Add init command.

--- src/metadata/bin/hasura.php
+++ src/metadata/bin/hasura.php
@@ -14,16 +14,25 @@ use Hasura\Metadata\Command\ClearMetadata;
 use Hasura\Metadata\Command\DropInconsistentMetadata;
 use Hasura\Metadata\Command\ExportMetadata;
 use Hasura\Metadata\Command\GetInconsistentMetadata;
+use Hasura\Metadata\Command\InitConfig;
 use Hasura\Metadata\Command\ReloadMetadata;
 use Hasura\Metadata\Manager;
 use Hasura\Metadata\ManagerInterface;
 use Hasura\Metadata\YamlOperator;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Filesystem\Filesystem;
 
 (new class() {
     private ArgvInput $input;
+
+    private ConsoleOutput $output;
+
+    private SymfonyStyle $symfonyStyle;
+
+    private Filesystem $filesystem;
 
     public function __construct()
     {
@@ -44,17 +53,24 @@ use Symfony\Component\Filesystem\Filesystem;
         }
 
         $this->input = new ArgvInput();
+        $this->output = new ConsoleOutput();
+        $this->symfonyStyle = new SymfonyStyle($this->input, $this->output);
+        $this->filesystem = new Filesystem();
     }
 
     public function run(): void
     {
         $app = new Application('Hasura Metadata');
         $app->addCommands($this->getCommands());
-        $app->run($this->input);
+        $app->run($this->input, $this->output);
     }
 
     private function getCommands(): array
     {
+        if ('init-config' === $this->input->getFirstArgument()) {
+            return [new InitConfig($this->filesystem)];
+        }
+
         $manager = $this->getManager();
 
         return [
@@ -63,6 +79,7 @@ use Symfony\Component\Filesystem\Filesystem;
             new DropInconsistentMetadata($manager),
             new ExportMetadata($manager),
             new GetInconsistentMetadata($manager),
+            new InitConfig($this->filesystem),
             new ReloadMetadata($manager),
         ];
     }
@@ -71,7 +88,7 @@ use Symfony\Component\Filesystem\Filesystem;
     {
         $config = $this->getConfig();
         $apiClient = new Client($config['baseUri'], $config['adminSecret']);
-        $yamlOperator = new YamlOperator(new Filesystem());
+        $yamlOperator = new YamlOperator($this->filesystem);
 
         return new Manager($apiClient, $config['metadataPath'], $yamlOperator);
     }
@@ -89,7 +106,7 @@ use Symfony\Component\Filesystem\Filesystem;
             }
         }
 
-        if (\file_exists($file)) {
+        if ($this->filesystem->exists($file)) {
             $config = require_once $file;
 
             if (!\is_array($config)) {
@@ -102,20 +119,30 @@ use Symfony\Component\Filesystem\Filesystem;
         $baseUri = $config['baseUri'] ?? $_ENV['HASURA_BASE_URI'] ?? $_SERVER['HASURA_BASE_URI'] ?? null;
         $adminSecret = $config['adminSecret'] ?? $_ENV['HASURA_ADMIN_SECRET'] ?? $_SERVER['HASURA_ADMIN_SECRET'] ?? null;
         $metadataPath = $config['metadataPath'] ?? $_ENV['HASURA_METADATA_PATH'] ?? $_SERVER['HASURA_METADATA_PATH'] ?? null;
+        $hintMessage = '(Use `init-config` command to generate config file).';
 
         if (null === $baseUri) {
-            throw new \RuntimeException(
-                sprintf('You should be config Hasura base uri via `%s` config file or `HASURA_BASE_URI` env.', $file)
+            $this->symfonyStyle->warning(
+                sprintf(
+                    'You should be config Hasura base uri via `%s` config file or `HASURA_BASE_URI` env. %s',
+                    $file,
+                    $hintMessage
+                )
             );
+
+            exit(1);
         }
 
         if (null === $metadataPath) {
-            throw new \RuntimeException(
+            $this->symfonyStyle->warning(
                 sprintf(
-                    'You should be config Hasura metadata path where to store metadata files via `%s` config file or `HASURA_METADATA_PATH` env.',
-                    $file
+                    'You should be config Hasura metadata path where to store metadata files via `%s` config file or `HASURA_METADATA_PATH` env. %s',
+                    $file,
+                    $hintMessage
                 )
             );
+
+            exit(1);
         }
 
         return [
