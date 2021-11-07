c7b958c0c8b00b91fd6e6a431adfd154f0e601fa
Minh Vuong <vuongxuongminh@gmail.com>
2021-11-07 15:48:17 +0700

    feat: add docusaurus

---
+++ docusaurus/docs/intro.md
@@ -0,0 +1,18 @@
+---
+sidebar_position: 1
+slug: /
+---
+
+# Introduction
+
+Set of PHP libraries to build the modern API with [Hasura](https://hasura.io) graphql engine.
+
+## Features
+
++ Manage Hasura metadata.
++ Event dispatcher to dispatch [events triggered](https://hasura.io/docs/latest/graphql/core/event-triggers/index.html) by Hasura.
++ Authentication hook ([webhooks auth mode](https://hasura.io/docs/latest/graphql/core/auth/authentication/webhook.html)).
++ Integration with [GraphQLite](https://graphqlite.thecodingmachine.io/) to build Hasura [remote schema](https://hasura.io/docs/latest/graphql/core/remote-schemas/index.html) to handle business logic.
++ Integration with [Sailor](https://github.com/spawnia/sailor) typesafe graphql client to interact with Hasura graphql api.
+
+
---
+++ docusaurus/docusaurus.config.js
@@ -0,0 +1,76 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Hasura Extra',
+  tagline: 'Unofficial PHP libraries for Hasura graphql engine.',
+  url: 'https://hasura-extra.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  // favicon: 'img/favicon.ico',
+  organizationName: 'hasura-extra',
+  projectName: 'hasura-extra.github.io',
+  deploymentBranch: 'main',
+  trailingSlash: false,
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          editCurrentVersion: true,
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/hasura-extra/hasura-extra.github.io/edit/main/',
+          routeBasePath: '/'
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Hasura Extra',
+        // logo: {
+        //   alt: 'My Site Logo',
+        //   src: 'img/logo.svg',
+        // },
+        items: [
+          {
+            type: 'localeDropdown',
+            position: 'left',
+          },
+          {
+            href: 'https://github.com/hasura-extra',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Minh Vuong. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'vi'],
+  },
+};
+
+module.exports = config;
