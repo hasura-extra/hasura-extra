d9d0fded5aa6a81d5740c2bf9111f7eb7a735f36
Minh Vuong <38932626+vuongxuongminh@users.noreply.github.com>
2021-11-13 21:54:00 +0700

    feat: [bundle] add exception interface & test cases. (#46)

--- src/bundle/tests/LoadCommandTest.php
+++ src/bundle/tests/LoadCommandTest.php
@@ -10,6 +10,15 @@ declare(strict_types=1);
 
 namespace Hasura\Bundle\Tests;
 
+use Hasura\Metadata\Command\ApplyMetadata;
+use Hasura\Metadata\Command\ClearMetadata;
+use Hasura\Metadata\Command\DropInconsistentMetadata;
+use Hasura\Metadata\Command\ExportMetadata;
+use Hasura\Metadata\Command\GetInconsistentMetadata;
+use Hasura\Metadata\Command\PersistState;
+use Hasura\Metadata\Command\ReloadMetadata;
+use Hasura\SailorBridge\Command\Codegen;
+use Hasura\SailorBridge\Command\Introspect;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 
 final class LoadCommandTest extends KernelTestCase
@@ -19,13 +28,57 @@ final class LoadCommandTest extends KernelTestCase
         $app = new Application(self::bootKernel());
 
         $this->assertTrue($app->has('hasura:metadata:apply'));
+        $this->assertInstanceOf(
+            ApplyMetadata::class,
+            $app->get('hasura:metadata:apply')->getCommand()
+        );
+
         $this->assertTrue($app->has('hasura:metadata:clear'));
+        $this->assertInstanceOf(
+            ClearMetadata::class,
+            $app->get('hasura:metadata:clear')->getCommand()
+        );
+
         $this->assertTrue($app->has('hasura:metadata:drop-inconsistent'));
+        $this->assertInstanceOf(
+            DropInconsistentMetadata::class,
+            $app->get('hasura:metadata:drop-inconsistent')->getCommand()
+        );
+
         $this->assertTrue($app->has('hasura:metadata:export'));
+        $this->assertInstanceOf(
+            ExportMetadata::class,
+            $app->get('hasura:metadata:export')->getCommand()
+        );
+
         $this->assertTrue($app->has('hasura:metadata:get-inconsistent'));
+        $this->assertInstanceOf(
+            GetInconsistentMetadata::class,
+            $app->get('hasura:metadata:get-inconsistent')->getCommand()
+        );
+
         $this->assertTrue($app->has('hasura:metadata:reload'));
+        $this->assertInstanceOf(
+            ReloadMetadata::class,
+            $app->get('hasura:metadata:reload')->getCommand()
+        );
+
         $this->assertTrue($app->has('hasura:metadata:persist-state'));
+        $this->assertInstanceOf(
+            PersistState::class,
+            $app->get('hasura:metadata:persist-state')->getCommand()
+        );
+
         $this->assertTrue($app->has('hasura:sailor:introspect'));
+        $this->assertInstanceOf(
+            Introspect::class,
+            $app->get('hasura:sailor:introspect')->getCommand()
+        );
+
         $this->assertTrue($app->has('hasura:sailor:codegen'));
+        $this->assertInstanceOf(
+            Codegen::class,
+            $app->get('hasura:sailor:codegen')->getCommand()
+        );
     }
 }
