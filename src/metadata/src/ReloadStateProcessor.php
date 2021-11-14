85ac8198decf60ef94457072be0d5222b3b66181
Minh Vuong <vuongxuongminh@gmail.com>
2021-11-14 08:16:25 +0700

    refactor!: use manager to reload state.

--- src/metadata/src/ReloadStateProcessor.php
+++ src/metadata/src/ReloadStateProcessor.php
@@ -10,22 +10,14 @@ declare(strict_types=1);
 
 namespace Hasura\Metadata;
 
-use Hasura\ApiClient\Client;
-
 final class ReloadStateProcessor implements StateProcessorInterface
 {
-    public function __construct(private Client $client)
+    public function __construct(private ManagerInterface $manager)
     {
     }
 
     public function process(): void
     {
-        $this->client->metadata()->query(
-            'reload_metadata',
-            [
-                'reload_remote_schemas' => true,
-                'reload_sources' => true
-            ]
-        );
+        $this->manager->reload(true, true);
     }
 }
