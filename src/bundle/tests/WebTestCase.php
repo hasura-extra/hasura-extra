61548d6710ccca3188001d09df5fec0487d094f7
tai-vu <vcttai@gmail.com>
2022-03-01 09:02:43 +0700

    Modify docs.

--- src/bundle/tests/WebTestCase.php
+++ src/bundle/tests/WebTestCase.php
@@ -1,10 +1,4 @@
 <?php
-/*
- * (c) Tai Vu <vcttai@gmail.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
 
 declare(strict_types=1);
 
@@ -14,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as SymfonyWebTestCase;
 
 abstract class WebTestCase extends SymfonyWebTestCase
 {
-    protected string $projectDir = __DIR__ . '/Fixture';
+    protected string $projectDir = __DIR__.'/Fixture';
 
     public static function getKernelClass()
     {
